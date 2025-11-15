49514e782f1a4d043b2cfd134dc5caf2f80d80fa
felipenehuengutierrez <felipe.nehueng@gmail.com>
2025-11-15 02:31:11 +0000

    React hello corregido

--- src/js/components/Body.jsx
+++ src/js/components/Body.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Body = () => {
 
   return (
-    <div className="Futbol" style={{  }}>
+    <div className="">
       <div className="body">
         <h5 className="body-title">Futbol Mundial</h5>
         <p className="body-text">Bienvenidos a un espacio dedicado a cuatro figuras que han marcado la historia del fútbol moderno. En estas páginas, exploraremos las carreras de Lionel Messi, Cristiano Ronaldo, Arturo Vidal y Ronaldinho. Desde el genio inigualable de Messi y la implacable determinación de Ronaldo, hasta la garra y el corazón de Vidal, y la magia impredecible de Ronaldinho, cada uno de estos jugadores dejó una huella imborrable en el deporte. Descubra los récords, los momentos clave y las historias que definieron a una generación de leyendas del fútbol.</p>
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -13,33 +13,33 @@ const Home = (props) => {
 
 	let Futbolistas = [
 		{
-			nombre: "Cristiano Ronaldo", info: "El mejor jugador de futbol de la historia de Portugal", imagenUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Cristiano_Ronaldo_2018.jpg/250px-Cristiano_Ronaldo_2018.jpg", href: "https://es.wikipedia.org/wiki/Cristiano_Ronaldo"
+			nombre: "Cristiano ", info: "El mejor jugador de futbol de la historia de POR", imagenUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Cristiano_Ronaldo_2018.jpg/250px-Cristiano_Ronaldo_2018.jpg", href: "https://es.wikipedia.org/wiki/Cristiano_Ronaldo"
 		},
 		{
-			nombre: "Lionel Messi", info: "El mejor jugador de futbol de la historia de Argentina", imagenUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Lionel-Messi-Argentina-2022-FIFA-World-Cup_%28cropped%29.jpg/250px-Lionel-Messi-Argentina-2022-FIFA-World-Cup_%28cropped%29.jpg", href: "https://es.wikipedia.org/wiki/Lionel_Messi"
+			nombre: "Lionel Messi", info: "El mejor jugador de futbol de la historia de ARG", imagenUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Lionel-Messi-Argentina-2022-FIFA-World-Cup_%28cropped%29.jpg/250px-Lionel-Messi-Argentina-2022-FIFA-World-Cup_%28cropped%29.jpg", href: "https://es.wikipedia.org/wiki/Lionel_Messi"
 		},
 		{
-			nombre: "Arturo Vidal", info: "El mejor jugador de futbol de la historia de Chile", imagenUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Chile_VS._Australia_%2813%29_2017-6-25_Arturo_Vidal.jpg/250px-Chile_VS._Australia_%2813%29_2017-6-25_Arturo_Vidal.jpg", href: "https://es.wikipedia.org/wiki/Arturo_Vidal"
+			nombre: "Arturo Vidal", info: "El mejor jugador de futbol de la historia de CHI", imagenUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Chile_VS._Australia_%2813%29_2017-6-25_Arturo_Vidal.jpg/250px-Chile_VS._Australia_%2813%29_2017-6-25_Arturo_Vidal.jpg", href: "https://es.wikipedia.org/wiki/Arturo_Vidal"
 		},
 		{
-			nombre: "Ronaldinho", info: "El mejor jugador de futbol de la historia de Brasil", imagenUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Ronaldinho_in_2019.jpg/250px-Ronaldinho_in_2019.jpg", href: "https://es.wikipedia.org/wiki/Ronaldinho"
+			nombre: "Ronaldinho", info: "El mejor jugador de futbol de la historia de BRA", imagenUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Ronaldinho_in_2019.jpg/250px-Ronaldinho_in_2019.jpg", href: "https://es.wikipedia.org/wiki/Ronaldinho"
 		}
 
 	]
 	return (
-		<div className="">
+		<div className="landing container-fluid">
 			<Navbar />
-			
-			<div className="row m-4">
+			<div className="row">
 				<Body />
 				{Futbolistas.map((item, index) => (
-					<div className="col-lg-3 col-md-6 col-12">
+					<div className="col-lg-3 col-md-6 col-md-12">
 						<Card key={index} nombre={item.nombre} info={item.info} imagenUrl={item.imagenUrl} href={item.href} />
 
 					</div>
 				))}
+				<Footer />
 			</div>
-			<Footer />
+
 		</div>
 	);
 };
